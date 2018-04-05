b15f3121116d802a24208c3e808d4e29af9ef540
dvaJi <francisco_p@outlook.com>
2018-04-05 14:44:52 -0300

    fix: Corrección de portadas

--- application/controllers/api/v2.php
+++ application/controllers/api/v2.php
@@ -49,24 +49,29 @@ class V2 extends REST_Controller {
                     $comicDir = $this->READER_PATH . $comic->stub . "_" . $comic->uniqid . "/";
 
                     $image_path = $comicDir . $comic->thumbnail;
-                    $image_thumb = $comicDir . "thumb2_" . $comic->thumbnail;
-                    if (!file_exists($image_thumb)) {
-                        // LOAD LIBRARY
-                        $this->load->library('image_lib');
+					if ($comic->thumbnail != "") {
+						$image_thumb = $comicDir . "thumb2_" . $comic->thumbnail;
+						if (!file_exists($image_thumb)) {
+							// LOAD LIBRARY
+							$this->load->library('image_lib');
 
-                        // CONFIGURE IMAGE LIBRARY
-                        $config['image_library'] = 'gd2';
-                        $config['source_image'] = $image_path;
-                        $config['new_image'] = $image_thumb;
-                        $config['maintain_ratio'] = TRUE;
-                        $config['height'] = 390;
-                        $config['width'] = 300;
-                        $this->image_lib->initialize($config);
-                        $this->image_lib->resize();
-                        $this->image_lib->clear();
-                    }
+							// CONFIGURE IMAGE LIBRARY
+							$config['image_library'] = 'gd2';
+							$config['source_image'] = $image_path;
+							$config['new_image'] = $image_thumb;
+							$config['maintain_ratio'] = TRUE;
+							$config['height'] = 390;
+							$config['width'] = 300;
+							$this->image_lib->initialize($config);
+							$this->image_lib->resize();
+							$this->image_lib->clear();
+						}
+						
+						$result[$index]['thumb2'] = $image_thumb;
+					} else {
+						$result[$index]['thumb2'] = NULL;
+					}
 
-                    $result[$index]['thumb2'] = $image_thumb;
                     $index++;
                 }
             }
@@ -124,24 +129,28 @@ class V2 extends REST_Controller {
             $comicDir = $this->READER_PATH . $comic->stub . "_" . $comic->uniqid . "/";
 
             $image_path = $comicDir . $comic->thumbnail;
-            $image_thumb = $comicDir . "thumb2_" . $comic->thumbnail;
-            if (!file_exists($image_thumb)) {
-                // LOAD LIBRARY
-                $this->load->library('image_lib');
+			if ($comic->thumbnail != "") {
+				$image_thumb = $comicDir . "thumb2_" . $comic->thumbnail;
+				if (!file_exists($image_thumb)) {
+					// LOAD LIBRARY
+					$this->load->library('image_lib');
 
-                // CONFIGURE IMAGE LIBRARY
-                $config['image_library'] = 'gd2';
-                $config['source_image'] = $image_path;
-                $config['new_image'] = $image_thumb;
-                $config['maintain_ratio'] = TRUE;
-                $config['height'] = 390;
-                $config['width'] = 300;
-                $this->image_lib->initialize($config);
-                $this->image_lib->resize();
-                $this->image_lib->clear();
-            }
+					// CONFIGURE IMAGE LIBRARY
+					$config['image_library'] = 'gd2';
+					$config['source_image'] = $image_path;
+					$config['new_image'] = $image_thumb;
+					$config['maintain_ratio'] = TRUE;
+					$config['height'] = 390;
+					$config['width'] = 300;
+					$this->image_lib->initialize($config);
+					$this->image_lib->resize();
+					$this->image_lib->clear();
+				}
 
-            $result['thumb2'] = site_url() . $image_thumb;
+				$result['thumb2'] = $image_thumb;
+			} else {
+				$result['thumb2'] = NULL;
+			}
 
             // order in the beautiful [comic][chapter][teams][page]
             $result["chapters"] = array();
@@ -310,6 +319,8 @@ class V2 extends REST_Controller {
                 $chapter->where('team_id', $this->get('team_id'));
             if (is_numeric($this->get('joint_id')))
                 $chapter->where('joint_id', $this->get('joint_id'));
+			if (is_numeric($this->get('lang')))
+                $chapter->where('language', $this->get('lang'));
 
             // and we'll still give only one result
             $chapter->limit(1)->get();
@@ -345,228 +356,6 @@ class V2 extends REST_Controller {
         } else {
             // the chapter with that id doesn't exist
             $this->response(array('error' => _('Chapter could not be found')), 404);
-        }
-    }
-
-    /**
-     * Returns chapters per page from team ID
-     * Includes releases from joints too
-     *
-     * This is NOT a method light enough to lookup teams. use api/members/team for that
-     *
-     * Available filters: id (required), page, per_page (default:30, max:100), orderby
-     *
-     * @author Woxxy
-     */
-    function team_get() {
-        // get the single team by id or stub
-        if ($this->get('stub')) {
-            $team = new Team();
-            $team->where('stub', $this->get('stub'))->limit(1)->get();
-        } else {
-            // check that the id is at least a valid number
-            $this->_check_id();
-            $team = new Team();
-            $team->where('id', $this->get('id'))->limit(1)->get();
-        }
-
-        // team found?
-        if ($team->result_count() == 1) {
-            $result = array();
-            $result['team'] = $team->to_array();
-
-            // get joints to get also the chapters from joints
-            $joints = new Joint();
-            $joints->where('team_id', $team->id)->get();
-
-
-            $chapters = new Chapter();
-            // get all chapters with the team ID
-            $chapters->where('team_id', $team->id);
-            foreach ($joints->all as $joint) {
-                // get also all chapters with the joints by the team
-                $chapters->or_where('joint_id', $joint->joint_id);
-            }
-
-            // filter for the page and the order
-            $this->_orderby($chapters);
-            $this->_page_to_offset($chapters);
-            $chapters->get();
-            $chapters->get_comic();
-
-            // let's save some power by reusing the variables we already have for team
-            // and put everything in the usual clean [comic][chapter][teams]
-            $result['chapters'] = array();
-            foreach ($chapters->all as $key => $chapter) {
-                if (!$chapter->team_id) {
-                    $chapter->get_teams();
-                    foreach ($chapter->teams as $item) {
-                        $result['chapters'][$key]['teams'][] = $item->to_array();
-                    }
-                } else {
-                    $result['chapters'][$key]['teams'][] = $team->to_array();
-                }
-                $result['chapters'][$key]['comic'] = $chapter->comic->to_array();
-                $result['chapters'][$key]['chapter'] = $chapter->to_array();
-            }
-
-            // all good
-            $this->response($result, 200); // 200 being the HTTP response code
-        } else {
-            // that single team id wasn't found
-            $this->response(array('error' => _('Team could not be found')), 404);
-        }
-    }
-
-    /**
-     * Returns chapters per page by joint ID
-     * Also returns the teams
-     *
-     * This is not a method light enough to lookup teams. use api/members/joint for that
-     *
-     * Available filters: id (required), page, per_page (default:30, max:100), orderby
-     *
-     * @author Woxxy
-     */
-    function joint_get() {
-        // check that the id is at least a valid number
-        $this->_check_id();
-
-        // grab by joint_id, id for joints means nothing much
-        $joint = new Joint();
-        $joint->where('joint_id', $this->get('id'))->limit(1)->get();
-
-        if ($joint->result_count() == 1) {
-            // good old get_teams() will give us all Team objects in an array
-            $team = new Team();
-            $teams = $team->get_teams(0, $this->get('id'));
-
-            // $teams is a normal array, so we have to do a loop
-            $result = array();
-            foreach ($teams as $item) {
-                $result['teams'][] = $item->to_array();
-            }
-
-            // grab all the chapters from the same joint
-            $chapters = new Chapter();
-            $chapters->where('joint_id', $joint->joint_id);
-
-            // apply the limit and orderby filters
-            $this->_orderby($chapters);
-            $this->_page_to_offset($chapters);
-            $chapters->get();
-            $chapters->get_comic();
-
-            // let's put the chapters in a nice [comic][chapter][teams] list
-            $result['chapters'] = array();
-            foreach ($chapters->all as $key => $chapter) {
-                $result['chapters'][$key]['comic'] = $chapter->comic->to_array();
-                $result['chapters'][$key]['chapter'] = $chapter->to_array();
-                $result['chapters'][$key]['teams'] = $result['teams'];
-            }
-
-            // all good
-            $this->response($result, 200); // 200 being the HTTP response code
-        } else {
-            // nothing for this joint or page
-            $this->response(array('error' => _('Team could not be found')), 404);
-        }
-    }
-
-    /**
-     * Returns all post and seleccted one by id and stub
-     *
-     * Available filters: page, per_page (default:30, max:100), orderby
-     *
-     * @author dvaJi
-     */
-    function blog_get() {
-        if ($this->get('id')) {
-            $this->_check_id();
-            $post = new Post();
-            $post->where('id', $this->get('id'))->limit(1)->get();
-            $post->get_users();
-        } else if ($this->get('stub')) {
-            $post = new Post();
-            $post->where('stub', $this->get('stub'));
-            $post->limit(1)->get();
-            $post->get_users();
-        } else {
-            $posts = new Post();
-            if ($this->get('lang')) {
-                $posts->where('language', $this->get('lang'));
-            }
-            // filter with orderby
-            $this->_orderby($posts);
-            // use page_to_offset function
-            $this->_page_to_offset($posts);
-            $posts->get();
-            $posts->get_users();
-        }
-
-        if ($post != null && $post->result_count() == 1) {
-            $result["data"]["post"] = $post->to_array();
-            $this->response($result, 200); // 200 being the HTTP response code
-        } else if ($posts != null && $posts->result_count() > 0) {
-            $result = array();
-            $result['data'] = array();
-            foreach ($posts->all as $key => $post) {
-                $result['data'][$key] = $post->to_array();
-            }
-
-            $this->response($result, 200); // 200 being the HTTP response code
-        } else {
-            // there's no post with that id
-            $this->response(array('error' => _('Post could not be found')), 404);
-        }
-    }
-
-    /**
-     * Returns all custompage and seleccted one by id and stub
-     *
-     * Available filters: page, per_page (default:30, max:100), orderby
-     *
-     * @author dvaJi
-     */
-    function pages_get() {
-        if ($this->get('id')) {
-            $this->_check_id();
-            $custompage = new Custompage();
-            $custompage->where('id', $this->get('id'))->limit(1)->get();
-            $custompage->get_users();
-        } else if ($this->get('stub')) {
-            $custompage = new custompage();
-            $custompage->where('stub', $this->get('stub'));
-            $custompage->limit(1)->get();
-            $custompage->get_users();
-        } else {
-            $custompages = new custompage();
-            if ($this->get('lang')) {
-                $custompages->where('language', $this->get('lang'));
-            }
-            // filter with orderby
-            $this->_orderby($custompages);
-            // use page_to_offset function
-            $this->_page_to_offset($custompages);
-            $custompages->get();
-        }
-
-        if ($custompage != null && $custompage->result_count() == 1) {
-            $result["data"]["custompage"] = $custompage->to_array();
-            $this->response($result, 200); // 200 being the HTTP response code
-        } else if ($custompages != null && $custompages->result_count() > 0) {
-            $result = array();
-            $result['data'] = array();
-            foreach ($custompages->all as $key => $custompage) {
-                // We only need those elements
-                $result['data'][$key]['title'] = $custompage->title();
-                $result['data'][$key]['stub'] = $custompage->stub;
-            }
-
-            $this->response($result, 200); // 200 being the HTTP response code
-        } else {
-            // there's no custompage with that id
-            $this->response(array('error' => _('custompage could not be found')), 404);
         }
     }
 
